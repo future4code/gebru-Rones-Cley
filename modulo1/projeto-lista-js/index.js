0807752aa50ad4d646a5177edfba6e630f895cf6
ronescl <rones_cley@hotmail.com>
2021-11-18 00:31:29 -0300

    Exercicio Strings Arrays

---
+++ modulo1/projeto-lista-js/index.js
@@ -0,0 +1,105 @@
+// 1 Indique todas as mensagens impressas no console, **SEM EXECUTAR o programa
+    
+    /*let array
+    console.log('a. ', array)
+    
+    array = null
+    console.log('b. ', array)
+    
+    array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    console.log('c. ', array.length)
+    
+    let i = 0
+    console.log('d. ', array[i])
+    
+    array[i+1] = 19
+    console.log('e. ', array)
+    
+    const valor = array[i+6]
+    console.log**('f. ', valor)*/
+    
+    //Exercicio 1
+
+    
+    //1 -Leia o código abaixo com atenção 
+    
+
+   //const frase = prompt("Digite uma frase")
+
+   //console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+   
+   /*const frase = "digite uma frase aqui"
+   const viagem = "Subi num onibus em Marrocos"
+   console.log (prompt(viagem.toUppercase())*/
+    
+    
+    
+     //console.log (frase.toUpperCase())
+
+    /* const frase = "Subi num onibus em Marrocos"
+    const novaFrase = frase.replaceAll ("A" , "I")
+    console.log ("frase antiga" , frase)  */
+
+
+   //const frase = "Subi num onibus em Marrocos"
+    //console.log ("a frase tem" + frase.length + "caracteres" )
+
+    
+   // Qual será o valor impresso no console se a entrada do usuário for: `""`?
+   //O valor impresso no console é 27
+
+
+  
+   //Exercícios de escrita de código
+
+
+   //1-Faça um programa que pergunte ao usuário seu nome e seu e-mail
+    //Em seguida, Imprima no console a seguinte mensagem: 
+
+   /*/ O e-mail emailDoUsuario foi cadastrado com sucesso. 
+    Seja bem-vinda(o), nomeDoUsuario!*/
+    
+    
+    
+    /*const usuario = prompt ("qual é o seu nome?")
+    const email = prompt ("qual é o seu email?")
+    console.log (`O usuario ${usuario} foi cadastrado com sucesso. Seja bem vinda ${email}`);*/
+
+    //Exercicio 2
+
+    /*const comidasPreferidas = ["lasanha" , "escondidinho" , "frango" , "picanha" , "tropeiro"];
+    console.log ("lasanha" , "escondidinho" , "frango" , "picanha" , "tropeiro")
+ 
+   
+    console.log (comidasPreferidas[0]);
+    console.log (comidasPreferidas[1]);
+    console.log (comidasPreferidas[2]);
+    console.log (comidasPreferidas[3]);
+    console.log (comidasPreferidas[4]);
+
+    console.log (prompt("qual é a sua comida preferida?"))
+
+    const frase = "minha comida preferida é quiabo"
+    const novaFrase = frase.replaceAll ("quiabo" , "escondidinho")*/
+
+    //exercício 3
+
+    /*const listaDeTarefas = [];
+
+    console.log (prompt ("Armazene 3 tarefas"))
+
+    const tarefas = ["acordar" , "correr" , "dormir"];
+
+    console.log (prompt("diga o indice de uma atividade que realizou hoje"))
+     
+    meuArray = [acordar,correr,dormir]
+      
+    meuArray = ["acordar" , "correr" , "dormir"]
+    meuArray.splice(i, 1)
+    console.log = prompt ("digite um numero")*/
+
+    //const tarefas = meuArray
+    //console.log (meuArray)
+
+
+
